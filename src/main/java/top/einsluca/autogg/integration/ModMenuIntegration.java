787f2d4660acd292c65135769ef8602330df9afa
Luca Arians <git@lucaaa.dev>
2026-08-28 23:22:17 +0200

    Port to Minecraft 26.2 (unobfuscated, no more Mojang mappings)
    Bump minecraft/loader/loom/fabric-api/cloth-config/modmenu to their
    26.2-compatible versions, switch the Loom plugin from
    fabric-loom-remap to fabric-loom (Mojang stopped shipping mappings
    from 26.1 onward since the game is now unobfuscated), drop the
    mappings block and mod*/mod-prefixed configurations accordingly, and
    bump the Java target to 25. Fixes ModMenuIntegration, which used
    AutoConfig.getConfigScreen that moved to AutoConfigClient in the new
    cloth-config release.

--- src/main/java/top/einsluca/autogg/integration/ModMenuIntegration.java
+++ src/main/java/top/einsluca/autogg/integration/ModMenuIntegration.java
@@ -2,7 +2,7 @@ package top.einsluca.autogg.integration;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import top.einsluca.autogg.config.AutoggConfig;
@@ -12,6 +12,6 @@ public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(AutoggConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(AutoggConfig.class, parent).get();
     }
 }
